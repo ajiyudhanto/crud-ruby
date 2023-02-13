69b883fb294ac39e8addf1be97b3538a6bbdfe56
aji yudhanto <aji.yudhanto@mekari.com>
2023-02-13 07:56:43 +0700

    Create CRUD for cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,30 @@
 class CardsController < InheritedResources::Base
+  before_action :set_card, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    description = params[:card][:description]
+    status = params[:card][:status]
+
+    @card.update(description: description, status: status)
+
+    redirect_to "/cards/#{@card.id}"
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to "/cards"
+  end
 
   private
+    def set_card
+      @card = Card.find(params[:card_id])
+    end
 
     def card_params
       params.require(:card).permit(:description, :status)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  resources :cards
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  resources :cards do
+    collection do
+      get '/' => 'cards#index'
+      post "/" => "cards#create", as: "create"
+      get "/new" => "cards#new", as: "new"
+      get "/:card_id" => "cards#show", as: "details"
+      get "/:card_id/edit" => "cards#edit", as: "edit"
+      patch "/:card_id" => "cards#update", as: "update"
+      delete "/:card_id" => "cards#destroy", as: "destroy"
+    end
+  end
+
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
